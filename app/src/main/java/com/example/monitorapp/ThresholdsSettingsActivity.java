b7d659d252cac5361211bed3ada9acfafc622667
xxu462 <94419600+xxu462@users.noreply.github.com>
2023-09-27 18:15:50 -0400

    number pad restriction updating

--- app/src/main/java/com/example/monitorapp/ThresholdsSettingsActivity.java
+++ app/src/main/java/com/example/monitorapp/ThresholdsSettingsActivity.java
@@ -5,7 +5,9 @@ package com.example.monitorapp;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.EditTextPreference;
@@ -54,6 +56,7 @@ public class ThresholdsSettingsActivity extends AppCompatActivity {
 
 
 
+
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -62,6 +65,8 @@ public class ThresholdsSettingsActivity extends AppCompatActivity {
             setupEditTextPreferences();
             setupResetButtonPreference();
         }
+
+
         private void setupEditTextPreferences() {
             String[] editTextKeys = {
                     "tempreture", "fault_temp_time", "fault_temp_num",
@@ -73,6 +78,7 @@ public class ThresholdsSettingsActivity extends AppCompatActivity {
 
             for (String key : editTextKeys) {
                 EditTextPreference editTextPreference = findPreference(key);
+                restrictOnlyNumericpad(editTextPreference);
                 if (editTextPreference != null) {
                     editTextPreference.setOnPreferenceChangeListener((preference, newValue) -> {
                         new Thread(() ->{
@@ -87,9 +93,7 @@ public class ThresholdsSettingsActivity extends AppCompatActivity {
                             ResultSet resultSet = DBConnection.getDataById(deviceID);
                             //Log.d(TAG,"result set: " + resultSet.toString());
                             if(resultSet.next()){
-
                                 String columnValue = resultSet.getString(key); // Replace key with the actual column name
-
                                 getActivity().runOnUiThread(() -> {
                                     editTextPreference.setText(columnValue);
                                 });
@@ -102,17 +106,32 @@ public class ThresholdsSettingsActivity extends AppCompatActivity {
                 }
             }
         }
+        private void restrictOnlyNumericpad(EditTextPreference editTextPreference){
+            editTextPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
+                    String stringValue = (String) newValue;
+                    try {
+                        // Attempt to parse the input as an integer
+                        int integerValue = Integer.parseInt(stringValue);
+                        // Input is a valid integer
+                        return true;
+                    } catch (NumberFormatException e) {
+                        // Input is not a valid integer, show an error message or reset the value
+                        Toast.makeText(getActivity(), "Please enter a valid integer", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }
+            });
+        }
         private void setupResetButtonPreference() {
             Preference resetButtonPreference = findPreference("reset_button");
             if (resetButtonPreference != null) {
                 resetButtonPreference.setOnPreferenceClickListener(preference -> {
-
                     new Thread(() -> {
                         //Log.d(TAG, "reset button clicked");
                         resetPreferencesToDefault();
                     }).start();
-
-
                     return true;
                 });
             }
@@ -120,7 +139,6 @@ public class ThresholdsSettingsActivity extends AppCompatActivity {
         private void updateAttributeValue(String attributeName, String newValue) {
             int deviceId = deviceID; // Replace with actual device ID
             Map<String, String> attributeUpdates = new HashMap<>();
-
             attributeUpdates.put(attributeName, newValue);
             DBConnection.updateAttributeValueById(deviceId, attributeUpdates);
         }
@@ -153,23 +171,14 @@ public class ThresholdsSettingsActivity extends AppCompatActivity {
                         }else{
                             Log.e(TAG, "cannot find designated preference");
                         }
-
                     }
                     DBConnection.updateAttributeValueById(deviceID, attributeUpdates);
-
-
-
-
                 }
-
                 resultSet.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-
-
-
     }
 
 }
